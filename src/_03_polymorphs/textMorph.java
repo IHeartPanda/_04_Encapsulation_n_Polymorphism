5ddb31d74c359426d35124db0fba67fed46e61f0
League <league@WTS9.attlocal.net>
2018-09-02 17:29:31 -0700

    added actionListener

--- src/_03_polymorphs/textMorph.java
+++ src/_03_polymorphs/textMorph.java
@@ -1,5 +1,26 @@
 package _03_polymorphs;
 
-public class textMorph {
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class textMorph extends Polymorph implements ActionListener{
+
+	protected textMorph(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
